1219b9aaa31c4a7ae5b5104f4dea636efdcfabb8
David Sadler <davidwallacesadler@gmail.com>
2020-03-17 15:55:17 -0600

    Add fragment classes and associated layout files for the plant creation flow

---
+++ app/src/main/java/com/davidsadler/bluepail/fragments/PlantListDialog.kt
@@ -0,0 +1,19 @@
+package com.davidsadler.bluepail.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.davidsadler.bluepail.R
+
+class PlantListDialog : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_plant_list_dialog, container, false)
+    }
+}
