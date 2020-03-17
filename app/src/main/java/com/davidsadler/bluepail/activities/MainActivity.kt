eb819923d8d7b2c9a0857b29eda73c6951f4b0c7
David Sadler <davidwallacesadler@gmail.com>
2020-03-17 17:45:12 -0600

    Add placeholder resources for toolbar menu items and the main fab, start implmenting basic navigation ui

--- app/src/main/java/com/davidsadler/bluepail/activities/MainActivity.kt
+++ app/src/main/java/com/davidsadler/bluepail/activities/MainActivity.kt
@@ -2,12 +2,43 @@ package com.davidsadler.bluepail.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.davidsadler.bluepail.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        val navController = Navigation.findNavController(this,R.id.navHostFragment)
+        setupActionBar(navController)
+        fab_create_plant.setOnClickListener {
+            navController.navigate(R.id.plantDetail)
+        }
+    }
+
+    private fun setupActionBar(navController: NavController) {
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = Navigation.findNavController(this,R.id.navHostFragment)
+        val navigated = NavigationUI.onNavDestinationSelected(item,navController)
+        return navigated || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(Navigation.findNavController(this,R.id.navHostFragment),null)
     }
 }
