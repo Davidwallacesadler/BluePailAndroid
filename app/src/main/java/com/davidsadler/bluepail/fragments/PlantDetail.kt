ee9fc7ea37bf98c60260e3b9558673cb2d6e8b01
David Sadler <davidwallacesadler@gmail.com>
2020-03-18 11:02:35 -0600

    Setup plantDetail ui

--- app/src/main/java/com/davidsadler/bluepail/fragments/PlantDetail.kt
+++ app/src/main/java/com/davidsadler/bluepail/fragments/PlantDetail.kt
@@ -1,19 +1,23 @@
 package com.davidsadler.bluepail.fragments
 
 import android.os.Bundle
+import android.view.*
+import android.view.View.inflate
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 import com.davidsadler.bluepail.R
+import kotlinx.android.synthetic.main.fragment_plant_detail.*
 
 class PlantDetail : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_plant_detail, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        toolbar_cancel_save.inflateMenu(R.menu.detail_toolbar)
     }
 }
