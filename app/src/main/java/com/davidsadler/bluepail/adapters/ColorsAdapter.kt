ca4a8eb847448ae997047a9c7ff6220cefa36c8d
David Sadler <davidwallacesadler@gmail.com>
2020-03-18 15:58:54 -0600

    Implement adapter for plant color selection recycler view along with associated ViewHolder and item view

---
+++ app/src/main/java/com/davidsadler/bluepail/adapters/ColorsAdapter.kt
@@ -0,0 +1,63 @@
+package com.davidsadler.bluepail.adapters
+
+import android.content.Context
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
+import com.davidsadler.bluepail.R
+import kotlinx.android.synthetic.main.item_color_list_cell.view.*
+
+interface OnColorSelectedListener {
+    fun colorSelected(colorId: Int)
+}
+
+class ColorsAdapter internal constructor(val context: Context, val colorSelectedListener: OnColorSelectedListener) : RecyclerView.Adapter<ColorsAdapter.ColorViewHolder>() {
+
+    private val inflater = LayoutInflater.from(context)
+    // Default color is red:
+    var selectedColor = ContextCompat.getColor(context,R.color.colorGroupRed)
+
+    private val mocColors = listOf<Int>(
+        R.color.colorGroupRed,
+        R.color.colorGroupOrange,
+        R.color.colorGroupYellow,
+        R.color.colorGroupGreen,
+        R.color.colorGroupBlue,
+        R.color.colorGroupPurple,
+        R.color.colorGroupMagenta)
+
+    inner class ColorViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val colorBackground : CardView = itemView.cardView_cell_color
+        val selectedOverlay : ImageView = itemView.imageView_selected_status
+        fun bind(colorId: Int, clickListener: OnColorSelectedListener, adapter: ColorsAdapter, position: Int) {
+            itemView.setOnClickListener {
+                clickListener.colorSelected(colorId)
+                selectedColor = colorId
+                adapter.notifyDataSetChanged()
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+       return mocColors.count()
+    }
+
+    override fun onBindViewHolder(holder: ColorViewHolder, position: Int) {
+        val colorId = mocColors[position]
+        val color = ContextCompat.getColor(context,colorId)
+        holder.colorBackground.setCardBackgroundColor(color)
+        holder.selectedOverlay.isVisible = color != selectedColor
+        holder.bind(color,colorSelectedListener, this, position)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ColorViewHolder {
+        val cellForRow = inflater.inflate(R.layout.item_color_list_cell, parent, false)
+        return ColorViewHolder(cellForRow)
+    }
+}
--- app/src/main/java/com/davidsadler/bluepail/fragments/PlantDetail.kt
+++ app/src/main/java/com/davidsadler/bluepail/fragments/PlantDetail.kt
@@ -4,11 +4,24 @@ import android.os.Bundle
 import android.view.*
 import android.view.View.inflate
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.davidsadler.bluepail.R
+import com.davidsadler.bluepail.adapters.ColorsAdapter
+import com.davidsadler.bluepail.adapters.OnColorSelectedListener
 import kotlinx.android.synthetic.main.fragment_plant_detail.*
 
-class PlantDetail : Fragment() {
+class PlantDetail : Fragment(), OnColorSelectedListener {
+
+    var selectedColor = 0
+
+    override fun colorSelected(colorId: Int) {
+        selectedColor = colorId
+        println("Selected Color: $selectedColor")
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -16,8 +29,21 @@ class PlantDetail : Fragment() {
         return inflater.inflate(R.layout.fragment_plant_detail, container, false)
     }
 
-    override fun onResume() {
-        super.onResume()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        inflateBottomToolbar()
+        setupColorRecyclerView()
+    }
+
+
+    private fun inflateBottomToolbar() {
         toolbar_cancel_save.inflateMenu(R.menu.detail_toolbar)
+    }
+
+    private fun setupColorRecyclerView() {
+        this.context?.let { context ->
+            recyclerView_plant_colors.layoutManager = GridLayoutManager(context,1,GridLayoutManager.HORIZONTAL,false)
+            recyclerView_plant_colors.adapter = ColorsAdapter(context,this)
+        }
     }
 }
